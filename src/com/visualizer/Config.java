15e8596196b85f8e2a2b26956e91db01982395da
Matthew Fong <jmattfong@gmail.com>
2016-03-01 01:17:49 -0800

    Use OpenCV and add some dummy files

---
+++ src/com/visualizer/Config.java
@@ -0,0 +1,15 @@
+/**
+ * Config.java
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * Copyright (C) 2016, Matthew Fong
+ */
+package com.visualizer;
+
+public class Config {
+
+	public final static int EYE_DISTANCE_MM = 65;
+
+}
---
+++ src/com/visualizer/ui/ImageViewer.java
@@ -0,0 +1,13 @@
+/**
+ * ImageViewer.java
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * Copyright (C) 2016, Matthew Fong
+ */
+package com.visualizer.ui;
+
+public class ImageViewer {
+
+}
